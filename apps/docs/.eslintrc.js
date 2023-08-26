0867884f41b471296e982d0536c18960a9890515
안형노 <hrahn@estsecurity.com>
2023-08-26 17:59:46 +0900

    docs eslint 코드 적용

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  // extends: 'next/core-web-vitals',
+  rules: {
+    // 'react/react-in-jsx-scope': 'off',
+    // 'jsx-a11y/anchor-is-valid': 'off',
+  },
+  globals: {
+    React: 'writable',
+  },
 };
--- packages/test.ts
+++
@@ -1,8 +0,0 @@
-export const isEven = (x: number) => x % 2 === 0;
-
-let abc = 1;
-
-const abcd = () => {
-  const a = 1;
-  return a;
-};
