15c74344d3caab8382e8f7febdf40bbe1bf34627
elle510 <elle0510@gmail.com>
2023-12-09 17:39:19 +0900

    storybook 설정 init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    // 'plugin:storybook/recommended', // 스토리북 설정하면서 자동생성됨. 일단 주석처리하고 자세한 것은 추후에 확인해보자
+  ],
   overrides: [],
   parserOptions: {
     project: './tsconfig.json',
