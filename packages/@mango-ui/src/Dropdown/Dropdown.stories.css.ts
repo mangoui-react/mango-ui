135d90dc1aaf4c7be20d752df8c6025961fee9f1
elle510 <elle0510@gmail.com>
2024-04-19 16:52:54 +0900

    feat: Dropdown 스토리북 구현

---
+++ packages/@mango-ui/src/Dropdown/Dropdown.stories.css.ts
@@ -0,0 +1,48 @@
+import { style } from '@vanilla-extract/css';
+
+export const content = style({
+  position: 'absolute',
+  backgroundColor: 'var(--component-bg)',
+  border: '0.0625rem solid var(--border)',
+  borderRadius: '0.25rem',
+  boxShadow: '0 0.125rem 0.1875rem 0 rgba(0, 0, 0, 0.15)',
+
+  ':focus': {
+    outline: 0,
+  },
+});
+
+export const item = style({
+  display: 'flex',
+  cursor: 'pointer',
+  padding: '8px 16px',
+  borderBottom: 'none',
+  lineHeight: '1.063rem', // rem(17px); 빌드시 에러남 (원래 안났는데 그냥 갑자기 남)
+
+  ':hover': {
+    background: 'var(--selected-hover)',
+  },
+
+  selectors: {
+    '&[data-disabled]': {
+      cursor: 'default',
+      opacity: 0.45,
+      pointerEvents: 'none',
+    },
+  },
+});
+
+export const group = style({
+  padding: '0.625rem 0.3125rem',
+});
+
+export const title = style({
+  paddingLeft: '0.3125rem',
+  paddingBottom: '0.5rem',
+});
+
+export const separator = style({
+  height: '0.0625rem',
+  backgroundColor: 'var(--border)',
+  margin: '0.3125rem',
+});
--- packages/@mango-ui/src/Dropdown/index.tsx
+++ packages/@mango-ui/src/Dropdown/index.tsx
@@ -4,17 +4,11 @@
 // export { default as DropdownContent } from './DropdownContent';
 // export { default as DropdownItem } from './DropdownItem';
 // export { default as DropdownClose } from './DropdownClose';
-// export { default as DropdownSeparator } from './DropdownSeparator';
-// export { default as DropdownGroup } from './DropdownGroup';
-// export { default as DropdownGroupTitle } from './DropdownGroupTitle';
 import Root from './Dropdown';
 import Close from './DropdownClose';
 import Content from './DropdownContent';
-import Group from './DropdownGroup';
-import GroupTitle from './DropdownGroupTitle';
 import Item from './DropdownItem';
 import Portal from './DropdownPortal';
-import Separator from './DropdownSeparator';
 import Trigger from './DropdownTrigger';
 
 export * from './Dropdown';
@@ -23,20 +17,14 @@ export * from './DropdownPortal';
 export * from './DropdownContent';
 export * from './DropdownItem';
 export * from './DropdownClose';
-export * from './DropdownSeparator';
-export * from './DropdownGroup';
-export * from './DropdownGroupTitle';
 
 const Dropdown = {
   Root,
   Trigger,
   Portal,
   Content,
-  Group,
-  GroupTitle,
   Item,
   Close,
-  Separator,
 };
 
 export default Dropdown;
