ebcda84a6b33beebd54d76cd3a5ba4f2c1989375
elle510 <elle0510@gmail.com>
2024-04-22 14:08:43 +0900

    feat: NumberInput 스토리북 구현

--- packages/@mango-ui/src/Modal/ModalCloseButton.tsx
+++ packages/@mango-ui/src/Modal/ModalCloseButton.tsx
@@ -5,7 +5,7 @@ import { ComponentPropsWithoutRef } from '../types/common';
 
 export interface ModalCloseButtonProps extends ComponentPropsWithoutRef<'button'> {}
 
-// TODO: 필요없을 듯...(좀 더 생각해 보고 결정하자)
+// TODO: 필요없을 듯...(좀 더 생각해 보고 결정하자) children 없애고 <CloseIcon /> 만 하자
 const ModalCloseButton = React.forwardRef<HTMLButtonElement, ModalCloseButtonProps>(
   (props, ref) => {
     const { children, ...rest } = props;
