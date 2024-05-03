6363d75cce49782d58d9716f75e6e5e37679f6bd
elle510 <elle0510@gmail.com>
2024-05-03 09:32:36 +0900

    feat: Toast 스토리북 구현

--- packages/@mango-ui/src/Toast/index.tsx
+++ packages/@mango-ui/src/Toast/index.tsx
@@ -6,28 +6,19 @@
 // export { default as ToastAction } from './ToastAction';
 // export { default as ToastClose } from './ToastClose';
 import Root from './Toast';
-import Action from './ToastAction';
 import Close from './ToastClose';
 import Content from './ToastContent';
-import Description from './ToastDescription';
-import Title from './ToastTitle';
 import Viewport from './ToastViewport';
 
 export * from './Toast';
 export * from './ToastContent';
 export * from './ToastViewport';
-export * from './ToastTitle';
-export * from './ToastDescription';
-export * from './ToastAction';
 export * from './ToastClose';
 
 const Toast = {
   Root,
-  Action,
   Close,
   Content,
-  Description,
-  Title,
   Viewport,
 };
 
