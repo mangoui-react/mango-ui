67b8c726f81dcbb3a0a78a6bb58d903773e0c994
elle510 <elle0510@gmail.com>
2024-06-23 12:46:21 +0900

    build: @melio-ui/react 빌드 스크립트

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -12,6 +12,7 @@ import {
 import path from 'path';
 import * as tsup from 'tsup';
 
+// @melio-ui/* build function
 async function build(packagePath) {
   const indexFile = `${packagePath}/src/index.ts`;
   if (!existsSync(indexFile)) return;
@@ -46,7 +47,7 @@ async function build(packagePath) {
   console.log('Built', packageName);
 }
 
-async function createPackageFile(packagePath) {
+async function createPackageFile(packagePath, addDependencies) {
   const buildPath = `${packagePath}/build`;
 
   const packageData = readFileSync(path.resolve(packagePath, './package.json'), 'utf8');
@@ -72,6 +73,13 @@ async function createPackageFile(packagePath) {
     types: './dist/index.d.ts',
   };
 
+  if (addDependencies) {
+    newPackageData.dependencies = {
+      ...newPackageData.dependencies,
+      ...addDependencies,
+    };
+  }
+
   const targetPath = path.resolve(buildPath, './package.json');
 
   writeFileSync(targetPath, JSON.stringify(newPackageData, null, 2), 'utf8');
@@ -80,6 +88,7 @@ async function createPackageFile(packagePath) {
   return newPackageData.name;
 }
 
+// @melio-ui/react build function
 async function buildReact(packagePath) {
   const buildPath = `${packagePath}/build`;
   const distPath = `${buildPath}/dist`;
@@ -89,7 +98,7 @@ async function buildReact(packagePath) {
 
   const indexFile = path.resolve(`${packagePath}/src`, './index.ts');
 
-  // index 파일 쓰기
+  // index 파일 쓰기 - @melio-ui
   writeFileSync(indexFile, newIndexContents, 'utf8');
 
   await tsup.build({
@@ -104,8 +113,20 @@ async function buildReact(packagePath) {
   });
   // console.log(`Built ${distPath}`);
 
+  // index 파일 쓰기 - ./ 으로 되돌리기
+  writeFileSync(indexFile, indexContents, 'utf8');
+
   // package.json 파일 생성
-  const packageName = await createPackageFile(packagePath);
+  const dependencies = {};
+  globSync(`${packagePath}/src/*`).forEach((dirPath) => {
+    const packageFile = `${dirPath}/package.json`;
+    if (!existsSync(packageFile)) return;
+
+    const packageData = readFileSync(packageFile, 'utf8');
+    const { version } = JSON.parse(packageData);
+    dependencies[`@melio-ui/${path.basename(dirPath)}`] = `^${version}`;
+  });
+  const packageName = await createPackageFile(packagePath, dependencies);
 
   // README.md 파일 copy
   await Promise.all(
@@ -113,9 +134,6 @@ async function buildReact(packagePath) {
       copyFileSync(file, `${buildPath}/${path.basename(file)}`);
     }),
   );
-
-  // index 파일 쓰기
-  writeFileSync(indexFile, indexContents, 'utf8');
 
   console.log('Built', packageName);
 }
@@ -128,15 +146,8 @@ void buildReact(packagePath);
 // @melio-ui/* build
 globSync(`${packagePath}/src/*`).forEach(build);
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 // globSync(`${packagePath}/src/tooltip`).forEach(build);
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-// globSync(`${packagePath}/src/use-portal`).forEach(build);
 
 // globSync(`${packagePath}/src/accordion`).forEach((path) => {
 //   console.log('=====test path', path);
 // });
-
-// const packages = glob.sync('*/index.{ts,tsx}', { cwd: `${packagePath}/src` });
-// const packages = glob.sync(packagePath);
-// console.log('====== packages: ', packages);
