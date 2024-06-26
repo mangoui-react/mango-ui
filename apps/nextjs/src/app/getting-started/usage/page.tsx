379333df776a3ab5f072dfc1c11f6a7ae629f7ee
elle510 <elle0510@gmail.com>
2024-06-26 17:12:01 +0900

    refactor: nextjs 필요없는 파일 제거

--- apps/nextjs/src/app/getting-started/usage/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function UsagePage(): JSX.Element {
-  return <div>UsagePage</div>;
-}
--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,8 +1,8 @@
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-import Layout from '@/components/Layout';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +15,58 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout>{children}</Layout>
+        <div className="flex w-full">
+          <nav className="shrink-0 z-[1300]">
+            <div
+              className="w-[260px] whitespace-nowrap box-border overflow-x-hidden shadow-none"
+              style={{
+                borderRight: '1px solid rgb(60, 60, 60)',
+                transition: 'width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+              }}
+            >
+              <div
+                className="bg-black text-inherit overflow-y-auto flex flex-col h-full z-[1200] fixed top-0 left-0 outline-0 w-[260px] overflow-x-hidden shadow-none"
+                style={{
+                  borderRight: '1px solid rgb(60, 60, 60)',
+                  transition: 'width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+                }}
+              >
+                <div className="grow h-full overflow-hidden p-5">
+                  Components
+                  <ul className="pt-5">
+                    <li>
+                      <Link href="/">Home</Link>
+                    </li>
+
+                    <li>
+                      <Link href="/components/avatar">Avatar</Link>
+                    </li>
+                    <li>
+                      <Link href="/components/button">Button</Link>
+                    </li>
+                    <li>
+                      <Link href="/components/accordion">Accordion</Link>
+                    </li>
+                    <li>
+                      <Link href="/components/input">Input</Link>
+                    </li>
+                    <li>
+                      <Link href="/components/textarea">Textarea</Link>
+                    </li>
+                    <li>
+                      <Link href="/components/number-input">NumberInput</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+          {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"></main> */}
+          <main className="w-full grow p-6">
+            <div className="relative flex items-center px-6 py-2 min-h-[60px]" />
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
