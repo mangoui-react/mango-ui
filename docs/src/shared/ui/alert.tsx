654af4826e3d26771ecc723042a99d9ac1f3094b
elle510 <elle0510@gmail.com>
2024-07-10 21:27:05 +0900

    docs: alert mdx examples 작성

---
+++ docs/src/shared/ui/alert.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { AlertContent, AlertRoot, AlertStatusIcon } from '@melio-ui/alert';
+
+import { cn } from '@/shared/utils/cn';
+
+interface RootProps extends React.ComponentPropsWithoutRef<typeof AlertRoot> {}
+function Root({ className, ...props }: RootProps) {
+  return (
+    <AlertRoot
+      {...props}
+      className={cn(
+        'flex items-center rounded-md px-4 py-1',
+        'bg-blue-400 text-white',
+        'data-[status=success]:bg-green-400 data-[status=success]:text-green-950',
+        'data-[status=warning]:bg-orange-400 data-[status=warning]:text-orange-950',
+        'data-[status=error]:bg-red-400 data-[status=error]:text-red-950',
+        className,
+      )}
+    />
+  );
+}
+
+interface StatusIconProps extends React.ComponentPropsWithoutRef<typeof AlertStatusIcon> {}
+function StatusIcon({ className, ...props }: StatusIconProps) {
+  return <AlertStatusIcon {...props} className={cn('px-0 py-2 font-bold', 'h-9 w-9', className)} />;
+}
+
+interface ContentProps extends React.ComponentPropsWithoutRef<typeof AlertContent> {}
+function Content({ className, ...props }: ContentProps) {
+  return <AlertContent {...props} className={cn('px-0 py-2 font-medium', className)} />;
+}
+
+export const Alert = {
+  Root,
+  StatusIcon,
+  Content,
+};
--- docs/src/shared/ui/index.tsx
+++ docs/src/shared/ui/index.tsx
@@ -1,3 +1,4 @@
 export { Accordion } from './accordion';
+export { Alert } from './alert';
 export { Button } from './Button';
 export { Input } from './Input';
